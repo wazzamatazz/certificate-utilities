621a12b2a87ad8df076e9ddfcaa942e2317d6783
Graham Watts <grahamcharleswatts@gmail.com>
2024-09-20 10:07:13 +0300

    Unit tests and certificate path parsing (#7)
    * Remove thumbprint and add static parser
    
    Removes the Thumbprint property. Instead, the Subject property will be used to define the subject, distinguished name or thumbprint of a store certificate.
    
    Also adds a property that specifies if the private key is required when loading a store certificate, and adds a static CreateFromPath method that can parse a file system or certificate store path.
    
    * Load from store changes
    
    The Subject for a location can now be the certificate thumbprint, distinguished name, or partial subject name.
    
    The location can also now specify if a private key is required when resolving a store certificate.
    
    * Add unit tests
    
    * Update readme
    
    * Update build.cake
    
    * Update version.json

--- src/Jaahas.CertificateUtilities/CertificateLocation.cs
+++ src/Jaahas.CertificateUtilities/CertificateLocation.cs
@@ -1,23 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace Jaahas.CertificateUtilities {
 
     /// <summary>
     /// Describes a file- or store-based X.509 client certificate.
     /// </summary>
-    public sealed class CertificateLocation : IValidatableObject {
+    public sealed partial class CertificateLocation : IValidatableObject {
 
         /// <summary>
         /// Returns <see langword="true"/> if no certificate has been specified.
         /// </summary>
-        public bool IsEmpty => !IsFileCert && !IsStoreCert;
+        public bool IsEmpty => !IsFileCertificate && !IsStoreCertificate;
 
         // File
 
         /// <summary>
         /// Returns <see langword="true"/> if a file system-based certificate has been specified.
         /// </summary>
-        public bool IsFileCert => !string.IsNullOrEmpty(Path);
+        public bool IsFileCertificate => !string.IsNullOrEmpty(Path);
 
 #if NET8_0_OR_GREATER
 
@@ -56,17 +57,16 @@ namespace Jaahas.CertificateUtilities {
         /// <summary>
         /// Returns <see langword="true"/> if a certificate store-based certificate has been specified.
         /// </summary>
-        public bool IsStoreCert => !string.IsNullOrEmpty(Subject) || !string.IsNullOrEmpty(Thumbprint);
+        public bool IsStoreCertificate => !string.IsNullOrEmpty(Subject);
 
         /// <summary>
-        /// The subject of the store certificate to use.
+        /// The subject name or thumbprint of the store certificate to use.
         /// </summary>
+        /// <remarks>
+        ///   The <see cref="Subject"/> can be a thumbprint, an X.500 distinguished name, or a 
+        ///   partial subject name for the certificate.
+        /// </remarks>
         public string? Subject { get; set; }
-
-        /// <summary>
-        /// The thumbprint of the store certificate to use.
-        /// </summary>
-        public string? Thumbprint { get; set; }
 
         /// <summary>
         /// The name of the certificate store to use.
@@ -79,16 +79,45 @@ namespace Jaahas.CertificateUtilities {
         public string? Location { get; set; }
 
         /// <summary>
-        /// When set, specifies if store-based certificates can be used even if they are invalid.
+        /// When set, specifies if a store-based certificate can be used even if it is invalid
+        /// (for example, if it has expired).
         /// </summary>
+        /// <remarks>
+        /// 
+        /// <para>
+        ///   This property is ignored unless <see cref="IsStoreCertificate"/> is <see langword="true"/>.
+        /// </para>
+        /// 
+        /// <para>
+        ///   If not set, invalid certificates are not allowed.
+        /// </para>
+        /// 
+        /// </remarks>
         public bool? AllowInvalid { get; set; }
+
+        /// <summary>
+        /// When set, specifies if a store-based certificate can be used even if its private key 
+        /// is not available.
+        /// </summary>
+        /// <remarks>
+        /// 
+        /// <para>
+        ///   This property is ignored unless <see cref="IsStoreCertificate"/> is <see langword="true"/>.
+        /// </para>
+        /// 
+        /// <para>
+        ///   If not set, private keys are required.
+        /// </para>
+        /// 
+        /// </remarks>
+        public bool? RequirePrivateKey { get; set; }
 
 
         /// <inheritdoc/>
         public override string ToString() {
             var parts = new List<string>();
 
-            if (IsFileCert) {
+            if (IsFileCertificate) {
                 if (!string.IsNullOrWhiteSpace(Path)) {
                     parts.Add($"Path={Path}");
                 }
@@ -98,7 +127,7 @@ namespace Jaahas.CertificateUtilities {
                 }
 #endif
             }
-            else if (IsStoreCert) {
+            else if (IsStoreCertificate) {
                 if (!string.IsNullOrWhiteSpace(Subject)) {
                     parts.Add($"Subject={Subject}");
                 }
@@ -118,9 +147,111 @@ namespace Jaahas.CertificateUtilities {
 
         /// <inheritdoc/>
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) {
-            if (IsFileCert && IsStoreCert) {
+            if (IsFileCertificate && IsStoreCertificate) {
                 yield return new ValidationResult("Certificate configuration can specify a file certificate or a store certificate, but not both.", new[] { nameof(Path), nameof(Subject) });
             }
+        }
+
+
+#if NET8_0_OR_GREATER
+
+        [GeneratedRegex(@"^CERT\:(?:\\|/)(?<location>.+?)(?:\\|/)(?<store>.+)(?:\\|/)(?<subject>.+)$", RegexOptions.IgnoreCase)]
+        private static partial Regex GetCertificateStorePathPatcher();
+
+#else
+
+        /// <summary>
+        /// Case-insensitive regex for matching the full path to a certificate in a certificate 
+        /// store e.g. <c>cert:\CurrentUser\My\1234567890abcdef1234567890abcdef123456789</c>. Both 
+        /// back- and forward-slashes can be used as path separators.
+        /// </summary>
+        private static readonly Regex s_certificateStorePathMatcher = new Regex(@"^CERT\:(?:\\|/)(?<location>.+?)(?:\\|/)(?<store>.+)(?:\\|/)(?<subject>.+)$", RegexOptions.IgnoreCase);
+
+        private static Regex GetCertificateStorePathPatcher() => s_certificateStorePathMatcher;
+
+#endif
+
+
+        /// <summary>
+        /// Parses a certificate location from a path string that can represent either a file 
+        /// system location or a certificate store location.
+        /// </summary>
+        /// <param name="path">
+        ///   The path string. This can be a file path or a path to a certificate in a certificate 
+        ///   store. See the method remarks for more information.
+        /// </param>
+        /// <returns>
+        ///   The parsed certificate location.
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///   <paramref name="path"/> is <see langword="null"/> or white space.
+        /// </exception>
+        /// <remarks>
+        /// 
+        /// <para>
+        ///   Certificate store locations can be specified using the following format: 
+        ///   <c>cert:\{location}\{store}\{thumbprint_or_subject}</c>
+        /// </para>
+        /// 
+        /// <para>
+        ///   The format is case-insensitive and supports both back- and forward-slashes as path 
+        ///   separators. Paths that do not match the above format are treated as file paths.
+        /// </para>
+        /// 
+        /// <para>
+        ///   Notes:
+        /// </para>
+        /// 
+        /// <list type="bullet">
+        ///   <item>
+        ///     <c>{location}</c> can be <c>CurrentUser</c> or <c>LocalMachine</c>.
+        ///   </item>
+        ///   <item>
+        ///     <c>{store}</c> is the name of the certificate store to use. See <see cref="System.Security.Cryptography.X509Certificates.StoreName"/> 
+        ///     for valid values.
+        ///   </item>
+        ///   <item>
+        ///     <c>{thumbprint_or_subject}</c> is the thumbprint, distinguished name, or partial 
+        ///     subject name for the certificate.
+        ///   </item>
+        /// </list>
+        /// 
+        /// <para>
+        ///   Examples:
+        /// </para>
+        /// 
+        /// <list type="bullet">
+        ///   <item>
+        ///     <c>cert:\CurrentUser\My\0123456789abcdef0123456789abcdef01234567</c> (find by thumbprint)
+        ///   </item>
+        ///   <item>
+        ///     <c>cert:\LocalMachine\My\CN=MyCert, O=MyOrg</c> (find by distinguished name)
+        ///   </item>
+        ///   <item>
+        ///     <c>cert:\LocalMachine\My\MyCert</c> (find by partial subject name)
+        ///   </item>
+        /// </list>
+        /// 
+        /// 
+        /// 
+        /// </remarks>
+        public static CertificateLocation CreateFromPath(string path) {
+            if (string.IsNullOrWhiteSpace(path)) {
+                throw new ArgumentOutOfRangeException(nameof(path));
+            }
+
+            var match = GetCertificateStorePathPatcher().Match(path);
+            if (match.Success) {
+                return new CertificateLocation() {
+                    Location = match.Groups["location"].Value,
+                    Store = match.Groups["store"].Value,
+                    Subject = match.Groups["subject"].Value
+                };
+            }
+
+            return new CertificateLocation() {
+                Path = path
+            };
         }
 
     }
--- src/Jaahas.CertificateUtilities/PACKAGE_README.md
+++ src/Jaahas.CertificateUtilities/PACKAGE_README.md
@@ -32,6 +32,32 @@ var certificateFromFile = loader.LoadCertificate(new CertificateLocation() {
 });
 ```
 
+When loading a certificate from a certificate store you can also specify if expired or invalid certificates can be returned, and whether or not the private key for the certificate must also be available. By default, expired or invalid certificates are not returned and the private key is required.
+
+```csharp
+var location = new CertificateLocation() {
+    Subject = "MyCertificate",
+    Store = "My",
+    Location = "CurrentUser",
+    AllowInvalid = true,
+    RequirePrivateKey = false
+};
+```
+
+You can also create a `CertificateLocation` instance from a path to a certificate file or certificate store location using the static `CertificateLocation.CreateFromPath` method. The method will parse your path to determine if it is a file system path or a certificate store path: 
+
+```csharp
+// Create from file path
+var location1 = CertificateLocation.CreateFromPath(@"C:\path\to\certificate.pfx");
+
+// Create from certificate store location
+var location2 = CertificateLocation.CreateFromPath(@"cert:\CurrentUser\My\localhost");
+```
+
+Certificate store locations can be specified using the format `cert:\{location}\{store}\{subject_or_thumbprint_or_distinguished_name}`. The format is case-insensitive and can use both back- and forward-slashes as path separators.
+
+When creating a `CertificateLocation` using `CreateFromPath`, remember that you may still need to set properties such as `Password` or `KeyPath` separately.
+
 On .NET 8.0 and higher, it is also possible to load certificates and private keys from separate PEM-encoded files:
 
 ```csharp
