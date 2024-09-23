1ecf096a0111abd3a7d97e4d3595a5bc74cc733f
Graham Watts <grahamcharleswatts@gmail.com>
2024-09-23 14:36:45 +0300

    Clarify default options behaviour

--- src/Jaahas.CertificateUtilities/CertificateLoaderOptions.cs
+++ src/Jaahas.CertificateUtilities/CertificateLoaderOptions.cs
@@ -8,6 +8,11 @@
         /// <summary>
         /// The base path to use when loading certificates from the file system.
         /// </summary>
+        /// <remarks>
+        ///   If <see cref="CertificateRootPath"/> is <see langword="null"/> or white space, the 
+        ///   <see cref="CertificateLoader"/> will use <see cref="AppContext.BaseDirectory"/> as 
+        ///   the default root path.
+        /// </remarks>
         public string? CertificateRootPath { get; set; }
 
     }
